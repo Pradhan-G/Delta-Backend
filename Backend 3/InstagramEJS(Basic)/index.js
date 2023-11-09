7066aea84710b7f7d1bea7c989e7bd7a3fa55797
Pradhan-G <abhishekpradhan12.10.00@gmail.com>
2023-11-09 21:56:52 +0530

    Instagram adding css and js files and serving them

--- Backend 3/InstagramEJS(Basic)/index.js
+++ Backend 3/InstagramEJS(Basic)/index.js
@@ -5,6 +5,7 @@ const app = express();
 const path = require("path");
 
 const port = 8000;
+app.use(express.static(path.join(__dirname, "public/css")));
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
@@ -13,18 +14,18 @@ app.get("/", (req, res) => {
   res.render("home.ejs");
 });
 
-app.get("*", (req, res) => {
-  res.render("nopagefound.ejs");
-});
-
 app.get("/ig/:username", (req, res) => {
   //We must import the data to use it
 
   const instaData = require("./data.json");
   let { username } = req.params;
-  let data = instaData[username];
-  console.log(data);
-  res.render("instagram.ejs", { data });
+  if (username == "cats" || username == "dogs") {
+    let data = instaData[username];
+    console.log(data);
+    res.render("instagram.ejs", { data });
+  } else {
+    res.render("nopagefound.ejs");
+  }
 });
 
 app.listen(port, () => {
